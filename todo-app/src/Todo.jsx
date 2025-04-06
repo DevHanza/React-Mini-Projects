f19357f7a8f9e1dacf3a5c256e8c3e3eaffeb1cf
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-04-06 13:09:13 +0000

    Complete Todo App

--- todo-app/src/Todo.jsx
+++ todo-app/src/Todo.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Todo() {
-  return <div>Todo</div>;
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = () => {
+    setTodos((todos) =>
+      todos.concat({
+        text: input,
+        id: crypto.randomUUID(),
+      })
+    );
+
+    setInput("");
+
+    console.log(todos);
+  };
+
+  const removeTodo = (id) => {
+    setTodos((todos) => todos.filter((t) => t.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="New ToDo"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+
+      <ul className="todos">
+        {todos.map(({ text, id }) => {
+          return (
+            <li className="todo" key={id}>
+              <span>{text}</span>
+              <button
+                className="close"
+                onClick={() => {
+                  removeTodo(id);
+                }}
+              >
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Todo;
