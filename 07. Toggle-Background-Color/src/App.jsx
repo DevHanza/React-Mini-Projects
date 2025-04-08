ddddfc8de0570a8d94319e15c562dd3923595f5f
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-04-08 15:47:38 +0000

    Toggle Background Color Project Basic Setup

---
+++ 07. Toggle-Background-Color/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ToggleBackgroundColor from "./ToggleBackgroundColor";
+
+function App() {
+  return <div>
+    <ToggleBackgroundColor/>
+  </div>;
+}
+
+export default App;
---
+++ 07. Toggle-Background-Color/src/ToggleBackgroundColor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ToggleBackgroundColor() {
+  return (
+    <div>
+      ToggleBackgroundColor
+    </div>
+  )
+}
+
+export default ToggleBackgroundColor
