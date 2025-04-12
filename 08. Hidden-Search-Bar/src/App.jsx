d836a34865bca1c32486fdb82d2b988eabec0021
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-04-12 10:22:43 +0000

    Complete 08. Hidden Search Bar

--- 08. Hidden-Search-Bar/src/App.jsx
+++ 08. Hidden-Search-Bar/src/App.jsx
@@ -1,1 +1,12 @@
-rfce
+import React from "react";
+import HiddenSearchBar from "./HiddenSearchBar";
+
+function App() {
+  return (
+    <div>
+      <HiddenSearchBar />
+    </div>
+  );
+}
+
+export default App;
