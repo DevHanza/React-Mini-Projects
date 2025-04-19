4e89dcf528df2cb0d6f5bb58d88793906a7fcb28
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-04-19 15:26:19 +0000

    Setup Envionment: E-Commerce Filter Advanced

---
+++ 12. E-Commerce Filter Advanced/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Products />
+      <Recommended />
+    </div>
+  );
+}
+
+export default App;
